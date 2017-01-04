e4efc8bf45bdfad89aab21f30385c649d9067824
Jeroen Breen <jeroen@innouveau.nl>
2017-01-04 15:54:58 +0100

    test file

---
+++ frontend/ui/modules/contacts/contact-detail/contacts-detail-controller-spec.js
@@ -0,0 +1,10 @@
+describe('contact detail', function () {
+
+    beforeEach(angular.mock.module('ofc.contactDetail'));
+
+    it('should see contacts', function () {
+        console.log('test');
+        expect(3).toBe(3);
+    });
+
+});
