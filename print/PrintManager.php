d2325e0e7d2b1d49483757d8d551d93afeedd22b
Jeroen Breen <jeroen@innouveau.nl>
2017-04-03 22:49:40 +0200

    design pdf

--- print/PrintManager.php
+++ print/PrintManager.php
@@ -12,7 +12,7 @@ require __DIR__ . '/../vendor/autoload.php';
 
 class PrintManager
 {
-    protected $data, $type, $year, $nr;
+    protected $data, $type, $year, $nr, $address;
 
     public function handlePrint()
     {
@@ -23,8 +23,9 @@ class PrintManager
         $dompdf = new Dompdf();
 
         $dompdf->loadHtml($this->getHMTL());
-//
+
         $dompdf->setPaper('A4', 'portrait');
+        $dompdf->set_option( 'dpi' , '150' );
         $dompdf->render();
 
         $link = "pdf/" . $this->type . "-" . $this->year . "-" . $this->nr . ".pdf";
@@ -42,9 +43,9 @@ class PrintManager
         $this->year = $this->data->date->year;
         $this->nr = $this->data->nr;
 
-        $address = (isset($this->data->client->address))? $this->data->client->address : '-';
+        $this->address = (isset($this->data->client->address))? $this->data->client->address : '-';
 
-        $doctype= $this->data->{'doctype'};
+        $doctype = $this->data->{'doctype'};
         if ($doctype == 'invoices') {
             $type = 'Factuur';
             $this->type = $type;
@@ -54,204 +55,239 @@ class PrintManager
             $this->type = $type;
         }
 
+
+        $html = "";
+        $html .= $this->getHead();
+        $html .= $this->getTop();
+        $html .= $this->getBody();
+        $html .= $this->getFooter();
+        $html .= $this->getClosing();
+        return $html;
+    }
+
+    protected function getHead()
+    {
         $html = "
-    <HTML>
-        <HEAD>
-            <TITLE></TITLE>
-            <meta charset='utf-8'>
-            <link rel='StyleSheet' href='style.css' type='text/css' media='all'>
-        </HEAD>
+             <HTML>
+                 <HEAD>
+                     <TITLE></TITLE>
+                     <meta charset='utf-8'>
+                     <link rel='StyleSheet' href='style.css' type='text/css' media='all'>
+                 </HEAD>
 
-        <BODY>
-            <table id='header-table'>
-                <tr>
-                    <td class='half'>
-                        <img class='header-logo' src='logo-print.png'>
-                    </td>
-                    <td id='info' class='half'>
-                        <b>" . $type . " " . $this->data->date->year . " - " . $this->data->nr . "</b><br>
-                        " . $this->data->date->day . " " . $date_month . " " . $this->data->date->year . "</td>
-                </tr>
-                <tr>
-                    <td>
-                        <div id='client'>
-                            <b>" . $this->data->client->name . "</b><br>
-                            " . $this->data->client->contactPerson . "<br>
-                            " . $address  . "<br>
-                            " . $this->data->client->zipcode . "
-                        </div>
-                    </td>
-                    <td>
-                        <div id='sender'>
-                            <b>Innouveau</b><br>
-                            " . $this->data->sender->contactPerson . "<br>
-                            " . $this->data->sender->address . "<br>
-                            " . $this->data->sender->zipcode . "
-                        </div>
-                    </td>
-                </tr>
-                <tr>
-                    <td colspan='2'>
-                        <div id='title'>
-                            <b>Betreft</b><br>
-                            " . $this->data->title . "
-                        </div>
-                    </td>
-                </tr>
-            </table>
+                 <BODY>
+                     <div class='pagewrap'>";
+        return $html;
+    }
 
-            <table id='lines-table'><tr>
-                    <td colspan='3' class='cell5'>
-                        <b>Werkzaamheden</b>
-                    </td>
-                </tr>";
+    protected function getTop()
+    {
+        $html = "
+            <div id='header'>
+                <table id='header-table'>
+                    <tr>
+                        <td valign='top' class='half'>
+                            <img id='header-logo' src='logo-print.png'>
+                        </td>
+                        <td class='header-table-spacer'>
+                            &nbsp;
+                        </td>
+                        <td valign='top' class='half'>
+                            <div id='document-info'>
+                                <b>" . $this->type . " " . $this->data->date->year . " - " . $this->data->nr . "</b><br>
+                                  " . $this->data->date->day . " " . $date_month . " " . $this->data->date->year . "
+                            </div>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td>
+                            <div id='client'>
+                                  <b>" . $this->data->client->name . "</b><br>
+                                  " . $this->data->client->contactPerson . "<br>
+                                  " . $this->address  . "<br>
+                                  " . $this->data->client->zipcode . "
+                            </div>
+                        </td>
+                        <td>
+                            &nbsp;
+                        </td>
+                        <td>
+                            <div id='sender'>
+                                  <b>Innouveau</b><br>
+                                  " . $this->data->sender->contactPerson . "<br>
+                                  " . $this->data->sender->address . "<br>
+                                  " . $this->data->sender->zipcode . "
+                            </div>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td colspan='3'>
+                            <div id='title'>
+                                  <b>Betreft</b><br>
+                                  " . $this->data->title . "
+                            </div>
+                        </td>
+                    </tr>
+                </table>
+            </div>";
+        return $html;
+    }
 
-        for ($i = 0; $i < count($this->data->lines); $i++) {
-            if ($this->data->lines[$i]->{'type'} == 'count') {
-                $title = $this->data->lines[$i]->{'title'};
-                $hours = $this->data->lines[$i]->{'hours'};
-                $rate = $this->data->lines[$i]->{'rate'};
-                $total += $rate * $hours;
-                $subtotal += $rate * $hours;
-                $html .= "
-                <tr>
-                    <td class='cell1'>
-                        " . $title . "
-                    </td>
-                    <td class='cell2'>
-                        " . $hours . " × " . $rate . " EUR
-                    </td>
-                    <td class='cell3'>
-                        " . $this->nrToCur($hours * $rate) . " EUR
-                    </td>
-                </tr>";
-            }
-            else if ($this->data->lines[$i]->{'type'} == 'amount') {
-                $title = $this->data->lines[$i]->{'title'};
-                $amount = $this->data->lines[$i]->{'amount'};
-                $total += $amount;
-                $subtotal += $amount;
-                $html .= "
-                <tr>
-                    <td colspan='2' class='cell4'>
-                        " . $title  . "
-                    </td>
-                    <td class='cell3'>
-                        " . $this->nrToCur($amount)  . " EUR
-                    </td>
-                </tr>";
-            }
-            else if ($this->data->lines[$i]->{'type'} == 'text') {
-                $title = $this->data->lines[$i]->{'text'};
-                $html .= "
-                <tr>
-                    <td colspan='3' class='cell5'>
-                        " . $title . "
-                    </td>
-                </tr>";
-            }
-            else if ($this->data->lines[$i]->{'type'} == 'enter') {
-                $html .= "
-                <tr>
-                    <td colspan='3' class='cell5 spacer'>
-                        &nbsp;
-                    </td>
-                </tr>";
-            }
-            else if ($this->data->lines[$i]->{'type'} == 'subtotal') {
-                $html .= "
-            <tr class='sub-spacer'>
-                <td colspan='2'></td>
-            </tr>
-            <tr class='subtotal-1'>
-                <td colspan='2' class='cell4'>
-                    <b>Subtotaal</b>
-                </td>
-                <td class='cell3 subtotal-sum'>
-                    <b>" . $this->nrToCur($subtotal)  . "</b> EUR
-                </td>
-            </tr>
-            <tr class='subtotal-2'>
-                <td colspan='2' class='cell4'>
-                    BTW 21%
-                </td>
-                <td class='cell3'>
-                    " . $this->nrToCur($subtotal * 0.21)  . " EUR
-                </td>
-            </tr>
-            <tr class='sub-spacer'>
-                <td colspan='2'></td>
-            </tr>";
-                $subtotal = 0;
-            }
-        }
+    protected function getFooter()
+    {
+        $html = "
+            <div id='footer'>
+                <div id='footer-text'>";
+                    if ($this->data->{'doctype'} === "invoices") {
+                        $html .= "Gelieve dit bedrag binnen 4 weken over te maken o.v.v. factuurnr en afzender op NL 68 ING B000 657 42 32 t.n.v. Innouveau, te Rotterdam<br>Innouveau | KvK 61118389 | BTW NL854214902B01";
+                    } else {
+                        $html .= "Handtekening voor akkoord:";
+                    }
+                    $html .= "
+                </div>
+            </div>";
+        return $html;
+    }
 
-        if (!$this->data->hideTotal){
-            $html .= "
+    protected function getClosing()
+    {
+        $html = "</div></BODY></HTML>";
+        return $html;
+    }
+
+    protected function getBody()
+    {
+        $html = "
+            <div id='lines'>
+                <table id='lines-table'>
+                    <tr>
+                        <td colspan='3' class='cell5'>
+                            <b>Werkzaamheden</b>
+                        </td>
+                    </tr>";
+
+            for ($i = 0; $i < count($this->data->lines); $i++) {
+                if ($this->data->lines[$i]->{'type'} == 'count') {
+                    $title = $this->data->lines[$i]->{'title'};
+                    $hours = $this->data->lines[$i]->{'hours'};
+                    $rate = $this->data->lines[$i]->{'rate'};
+                    $total += $rate * $hours;
+                    $subtotal += $rate * $hours;
+                    $html .= "
+                    <tr>
+                        <td class='cell1'>
+                            " . $title . "
+                        </td>
+                        <td class='cell2'>
+                            " . $hours . " × " . $rate . " EUR
+                        </td>
+                        <td class='cell3'>
+                            " . $this->nrToCur($hours * $rate) . " EUR
+                        </td>
+                    </tr>";
+                }
+                else if ($this->data->lines[$i]->{'type'} == 'amount') {
+                    $title = $this->data->lines[$i]->{'title'};
+                    $amount = $this->data->lines[$i]->{'amount'};
+                    $total += $amount;
+                    $subtotal += $amount;
+                    $html .= "
+                    <tr>
+                        <td colspan='2' class='cell4'>
+                            " . $title  . "
+                        </td>
+                        <td class='cell3'>
+                            " . $this->nrToCur($amount)  . " EUR
+                        </td>
+                    </tr>";
+                }
+                else if ($this->data->lines[$i]->{'type'} == 'text') {
+                    $title = $this->data->lines[$i]->{'text'};
+                    $html .= "
+                    <tr>
+                        <td colspan='3' class='cell5'>
+                            " . $title . "
+                        </td>
+                    </tr>";
+                }
+                else if ($this->data->lines[$i]->{'type'} == 'enter') {
+                    $html .= "
                     <tr>
                         <td colspan='3' class='cell5 spacer'>
                             &nbsp;
                         </td>
-                    </tr>
-                    <tr>
-                        <td colspan='2' class='cell4'>
-                            Totaal";
-
-            if ($type == "Offerte") {
-                $html .= " (excl. 21% BTW)";
-            }
-
-            $html .= "      </td>
-                        <td class='cell3'>
-                            " . $this->nrToCur($total) . " EUR
-                        </td>
                     </tr>";
-            if ($type == "Factuur" && $this->data->vat == false) {
-                $html .=
-                    "<tr>
-                        <td colspan='2' class='cell4'>
-                            BTW 21%
-                        </td>
-                        <td class='cell3'>
-                            " . $this->nrToCur($total * 0.21) . " EUR
-                        </td>
-                    </tr>
-                    <tr>
-                        <td colspan='2' class='cell4'>
-                            <b>Te betalen</b>
-                        </td>
-                        <td class='cell3'>
-                            <b>" . $this->nrToCur($total * 1.21) . " EUR</b>
-                        </td>
-                    </tr>";
-            }
-        }
-
-        $html .= "
-
-            </table>
-
-            <table id='footer-table'>
-                <tr>
-                    <td>
-                        <div id='footer'>";
-
-        if ($type == "Factuur") {
-            $html .= "Gelieve dit bedrag binnen 4 weken over te maken o.v.v. factuurnr en afzender op NL 68 ING B000 657 42 32 t.n.v. Innouveau, te Rotterdam<br>Innouveau | KvK 61118389 | BTW NL854214902B01";
-        }
-        if ($type == "Offerte") {
-            $html .= "Handtekening voor akkoord:";
-        }
-
-        $html .= "
-                        </div>
+                }
+                else if ($this->data->lines[$i]->{'type'} == 'subtotal') {
+                    $html .= "
+                <tr class='sub-spacer'>
+                    <td colspan='2'></td>
+                </tr>
+                <tr class='subtotal-1'>
+                    <td colspan='2' class='cell4'>
+                        <b>Subtotaal</b>
+                    </td>
+                    <td class='cell3 subtotal-sum'>
+                        <b>" . $this->nrToCur($subtotal)  . "</b> EUR
                     </td>
                 </tr>
-            </table>
+                <tr class='subtotal-2'>
+                    <td colspan='2' class='cell4'>
+                        BTW 21%
+                    </td>
+                    <td class='cell3'>
+                        " . $this->nrToCur($subtotal * 0.21)  . " EUR
+                    </td>
+                </tr>
+                <tr class='sub-spacer'>
+                    <td colspan='2'></td>
+                </tr>";
+                    $subtotal = 0;
+                }
+            }
 
-        </BODY>
-    </HTML>";
+            if (!$this->data->hideTotal){
+                $html .= "
+                        <tr>
+                            <td colspan='3' class='cell5 spacer'>
+                                &nbsp;
+                            </td>
+                        </tr>
+                        <tr>
+                            <td colspan='2' class='cell4'>
+                                Totaal";
 
+                if ($type == "Offerte") {
+                    $html .= " (excl. 21% BTW)";
+                }
+
+                $html .= "      </td>
+                            <td class='cell3'>
+                                " . $this->nrToCur($total) . " EUR
+                            </td>
+                        </tr>";
+                if ($type == "Factuur" && $this->data->vat == false) {
+                    $html .=
+                        "<tr>
+                            <td colspan='2' class='cell4'>
+                                BTW 21%
+                            </td>
+                            <td class='cell3'>
+                                " . $this->nrToCur($total * 0.21) . " EUR
+                            </td>
+                        </tr>
+                        <tr>
+                            <td colspan='2' class='cell4'>
+                                <b>Te betalen</b>
+                            </td>
+                            <td class='cell3'>
+                                <b>" . $this->nrToCur($total * 1.21) . " EUR</b>
+                            </td>
+                        </tr>";
+                }
+            }
+
+            $html .= "</table></div>";
         return $html;
     }
 
