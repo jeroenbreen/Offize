624cb552e7e30bed3153a6cae6ebed1e4d0ed88f
Jeroen Breen <>
2021-02-26 12:23:51 +0100

    Update config.js

--- public/config.js
+++ public/config.js
@@ -1,6 +1,6 @@
 window.config = {
-    api: 'https://office.innouveau.com/backend/',
-    printLocation: 'https://office.innouveau.com/print/'
-    //api: 'http://localhost/innouveau/office/dev/public/backend/',
-    //printLocation: 'http://localhost/innouveau/office/dev/print/'
+    //api: 'https://office.innouveau.com/backend/',
+    //printLocation: 'https://office.innouveau.com/print/'
+    api: 'http://localhost/innouveau/office/dev/public/backend/',
+    printLocation: 'http://localhost/innouveau/office/dev/print/'
 };
